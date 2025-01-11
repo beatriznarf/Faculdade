30df95fc7661146542b1d862373b62454070859e
Beatriz Narciso <147112758+beatriznarf@users.noreply.github.com>
2025-01-11 13:41:15 -0300

    Delete Python directory

--- Python/Primeira aplicação/Exercícios/Exercicio4.py
+++
@@ -1,5 +0,0 @@
-#4 - Imprima a frase: O valor arredondado de pi é: {pi_arredondado} em que o valor de pi 
-# precisa ser armazenado em uma variável e arredondado para apenas duas casas decimais. 
-
-pi=3.14
-print("Valor de PI: ", pi)
